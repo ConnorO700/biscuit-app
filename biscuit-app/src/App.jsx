1cd10ab21851208c061e9e262606dd28d5e1b75e
= <=>
2025-03-21 17:09:42 -0400

    not found page & react icons

--- biscuit-app/src/App.jsx
+++ biscuit-app/src/App.jsx
@@ -3,12 +3,13 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './Pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import BiscuitPage from './Pages/BiscuitPage'
-
+import NotFoundPage from './Pages/NotFound';
 function App() {
 	const router = createBrowserRouter(createRoutesFromElements(
 		<Route path='/' element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path='/biscuit' element={<BiscuitPage/>}/>
+			<Route path='*' element={<NotFoundPage />}/>
 		</Route>
 	));
 	return (
